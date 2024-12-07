99c05de6389fb5f04f067a673b56051e46b25bde
Abhinav <abhinavchaudhary2014@gmail.com>
2024-12-07 16:41:15 +0530

    exec type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.nio.file.*;
 
 public class Main {
 
@@ -26,9 +27,29 @@ public class Main {
         return false;
     }
 
+    static private String handleExec(String in) {
+        String PATH = System.getenv("PATH");
+        StringTokenizer path_tokens = new StringTokenizer(PATH, ":");
+        String dir = "";
+        while (path_tokens.hasMoreTokens()) {
+            dir = path_tokens.nextToken();
+            if (isExec(dir, in))
+                return dir;
+        }
+        return "";
+    }
+
+    static private boolean isExec(String dir, String file) {
+        Path path = Paths.get(dir, file);
+        if (Files.exists(path))
+            return true;
+        return false;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
+        System.out.println(args[0]);
 
         while (true) {
             input = scanner.nextLine();
@@ -46,6 +67,8 @@ public class Main {
                 case "type":
                     if (isBuiltin(input))
                         System.out.println(input + " is a shell builtin");
+                    else if (handleExec(input) != "")
+                        System.out.println(input + "is" + handleExec(input));
                     else
                         System.out.println(input + ": not found");
                     break;
